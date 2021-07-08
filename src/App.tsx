ff2820db81336e2f14e59af6ec22028e6af30e0e
Yuliya <yulia.podd@gmail.com>
2021-07-08 22:16:17 +0300

    init: start SA implementation

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Button from "./components/button/Button";
+
+function App() {
+  return <Button/>;
+}
+
+export default App;
---
+++ src/components/button/Button.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./button.scss"
+
+const Button: React.FC = () => (
+    <div className='button'>Button</div>
+);
+export default Button;
