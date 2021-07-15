cfa4095cfe9b38ff9c9575ec4a2b113a28716251
Yuliya <yulia.podd@gmail.com>
2021-07-15 12:44:48 +0300

    refactor: add formatting

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import Button from "./components/button/Button";
 import DropdownList from "./components/dropdown-list/dropdown";
 
 function App() {
-  return <>
-  <Button className='primary'/>
-  <DropdownList className='active'/>
-  </>;
+  return (
+    <>
+      <Button className="primary" />
+      <DropdownList className="active" />
+    </>
+  );
 }
 
 export default App;
