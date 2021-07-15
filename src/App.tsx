1a86b5a161afebb8df3a309dd64b40bb39caae37
Yuliya <yulia.podd@gmail.com>
2021-07-15 14:23:29 +0300

    refactor: remove components

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,7 @@ import Button from "./components/button/Button";
 import DropdownList from "./components/dropdown-list/dropdown";
 
 function App() {
-  return (
-    <>
-      <Button className="primary" />
-      <DropdownList className="active" />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
