eef2b9999be312e814f9eecda6118cce43c0257a
Yuliya <yulia.podd@gmail.com>
2021-07-27 12:19:37 +0300

    feat: add onchage type

--- src/components/DropdownList/DropdownList.stories.tsx
+++ src/components/DropdownList/DropdownList.stories.tsx
@@ -4,9 +4,16 @@ import { DropdownList } from "./DropdownList";
 export default {
   title: "Example/DropdownList",
   component: DropdownList,
+  argTypes: { handleChange: { action: 'changed' } }
 } as ComponentMeta<typeof DropdownList>;
 
-const Template: ComponentStory<typeof DropdownList> = (args) => <DropdownList {...args} />;
+const Template: ComponentStory<typeof DropdownList> = (args) => (
+  <DropdownList {...args} />
+);
 
 export const Default = Template.bind({});
-Default.args = { formName: 'type',labelHeader: 'Тип анализа', optionsArray: ['ABC анализ', 'другой анализ']};
+Default.args = {
+  formName: "type",
+  labelHeader: "Тип анализа",
+  optionsArray: ["ABC анализ", "другой анализ"]  
+};
--- src/components/DropdownList/DropdownList.tsx
+++ src/components/DropdownList/DropdownList.tsx
@@ -7,20 +7,22 @@ interface IDropdownItemProps {
   formName: string;
   labelHeader: string;
   optionsArray: string[];
+  handleChange: (arg: any) => void;
 }
 
 export function DropdownList({
   formName,
   labelHeader,
   optionsArray,
+  handleChange,
 }: IDropdownItemProps) {
-  let options = optionsArray.map(option => {
+  let options = optionsArray.map((option) => {
     return <option value={option}>{option}</option>;
   });
   return (
     <div className={styles.Dropdown}>
       <label htmlFor={formName}>{labelHeader}</label>
-      <select name={formName} id={formName}>
+      <select name={formName} id={formName} onChange={handleChange}>
         {options}
       </select>
     </div>
