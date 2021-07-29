a1cc389d50b41d7784bf8d2e2cacbb011ada3628
Yuliya <yulia.podd@gmail.com>
2021-07-29 18:34:04 +0300

    refactor: change default styling

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import Select from "react-select";
+import Select, { components } from "react-select";
+import caret from "./assets/caret.svg";
 
 import styles from "./Dropdown.module.scss";
 
@@ -9,6 +9,26 @@ interface IDropdownItemProps {
   optionsArray: { value: string; label: string }[];
   handleChange: (arg: any) => void;
 }
+const DropdownIndicator = (props: any) => {
+  return (
+    components.DropdownIndicator && (
+      <components.DropdownIndicator {...props}>
+        <img src={caret} alt='caret icon' className={styles.CaretIcon} />
+      </components.DropdownIndicator>
+    )
+  );
+};
+const rewrittenStyles = {
+  control: (provided: any) => ({
+    ...provided,
+    border: "1px solid rgba(0, 66, 105, 0,28)",
+    borderRadius: "2px",
+    marginTop: "4px",
+  }),
+  indicatorSeparator: () => ({
+    display: "none",
+  }),
+};
 export function Dropdown({
   dropdownName,
   labelText,
@@ -19,6 +39,9 @@ export function Dropdown({
     <div className={styles.Dropdown}>
       <label htmlFor={dropdownName}>{labelText}</label>
       <Select
+        components={{ DropdownIndicator }}
+        styles={rewrittenStyles}
+        className={styles.Select}
         name={dropdownName}
         defaultValue={optionsArray[0]}
         options={optionsArray}
